66509fcf3d662d92f9ffdf1e825de0ca0d4c5aac
yxm <1292784668@qq.com>
2020-09-28 17:29:00 +0800

    one

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+  provide: function() {
+    return {
+      dd: this.add,
+    };
+  },
+  components: {},
+  methods: {
+    add() {
+      return 2;
+    },
+  },
 };
 </script>
 
